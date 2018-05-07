b612c06ecfecd18c00a3a0c76b7e121a22e17ed1
Djibril NDIAYE <djibsone51@gmail.com>
2018-05-07 14:21:40 +0000

    add

--- resources/views/layouts/adminLayout/admin_design.blade.php
+++ resources/views/layouts/adminLayout/admin_design.blade.php
@@ -8,10 +8,10 @@
 <link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap-responsive.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/fullcalendar.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-style.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/backend_css/select2.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-media.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/backend_css/select2.css') }}" />
+<link href="font-awesome/{{ asset('fonts/backend_fonts/font-awesome.css') }}" rel="stylesheet" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/jquery.gritter.css') }}" />
-<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso18.css" /> 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -28,10 +28,10 @@
 <script src="{{ asset('js/backend_js/bootstrap.min.js') }}"></script> 
 <script src="{{ asset('js/backend_js/jquery.uniform.js') }}"></script> 
 <script src="{{ asset('js/backend_js/select2.min.js') }}"></script> 
-<script src="{{ asset('js/backend_js/jquery.dataTables.min.js') }}"></script> 
 <script src="{{ asset('js/backend_js/jquery.validate.js') }}"></script> 
 <script src="{{ asset('js/backend_js/matrix.js') }}"></script> 
+<script src="{{ asset('js/backend_js/jquery.dataTables.min.js') }}"></script> 
+<script src="{{ asset('js/backend_js/matrix.tables.js') }}"></script> 
 <script src="{{ asset('js/backend_js/matrix.form_validation.js') }}"></script>
-<script src="{{ asset('js/backend_js/matrix.tables.js') }}"></script>
 </body>
 </html>
--- resources/views/layouts/adminLayout/admin_sidebar.blade.php
+++ resources/views/layouts/adminLayout/admin_sidebar.blade.php
@@ -1,26 +1,26 @@
 <!--sidebar-menu-->
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
-    <li class="active"><a href="{{('../admin/dashboard')}}"><i class="icon icon-home"></i> <span> <strong>Accueil</strong> </span></a> </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-signal"></i> <span><strong>Echantillons</strong> </span> <span class="label label-important">2</span> </a>
-     <ul>
-        <li><a href="{{ url('/admin/add-echantillon') }}">Ajouter échantillons</a></li>
-        <li><a href="{{ url('/admin/voir-echantillon') }}">Consulter échantillons</a></li>
-      </ul> 
-    </li>
-    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span><strong>Analyses &amp; Tests</strong> </span></a> </li>
-    <li><a href="tables.html"><i class="icon icon-th"></i> <span><strong>Equipements</strong> </span></a></li>
-    <li><a href="grid.html"><i class="icon icon-fullscreen"></i> <span><strong>Stock</strong> </span></a></li>
-    <li class="submenu"><a href="#"><i class="icon icon-th-list"></i> <span><strong>Utilisateurs &amp; Personnel</strong> 
-      <ul>
-        <li><a href="form-common.html"><strong>Accueil</strong> </a></li>
-        <li><a href="form-validation.html">Form with Validation</a></li>
-        <li><a href="form-wizard.html">Form with Wizard</a></li>
+    <li class="active"><a href="{{('/admin/dashboard')}}"><i class="icon icon-home"></i> <span> <strong>Accueil</strong> </span></a> </li>
+    <li class="submenu"> <a href="#"><i class="icon icon-signal"></i> <span><strong>Echantillons</strong> </span></a> 
+        <ul>
+          <li><a href="#"><strong>Ajouter Echantillons</strong> </a></li>
+          <li><a href="{{('../admin/echantillons/index_echan')}}">Listes Ecahntillons</a></li>
+          <li><a href="#">Autres</a></li>
       </ul>
     </li>
-    <li><a href="buttons.html"><i class="icon icon-tint"></i><strong>Stock &amp; Fournisseurs</strong> </a></li>
+    <li> <a href="#"><i class="icon icon-inbox"></i> <span><strong>Analyses &amp; Tests</strong> </span></a> </li>
+    <li><a href="#"><i class="icon icon-th"></i> <span><strong>Equipements</strong> </span></a></li>
+    <li><a href="{{('/stocks')}}"><i class="icon icon-fullscreen"></i> <span><strong>Stock</strong> </span></a></li>
+    <li class="submenu"><a href="#"><i class="icon icon-th-list"></i> <span><strong>Utilisateurs &amp; Personnel</strong> 
+      <ul>
+        <li><a href="/users/listeperso"><strong>Personnel</strong> </a></li>
+        <li><a href="#">Unites Techniques</a></li>
+        <li><a href="#">Autres</a></li>
+      </ul>
+    </li>
+    <li><a href="buttons.html"><i class="icon icon-tint"></i><strong>Clients &amp; Fournisseurs</strong> </a></li>
     <li><a href="interface.html"><i class="icon icon-pencil"></i> <span><strong>Tableau de bord</strong></span></a></li>
   </ul>
 </div>
-
 <!--sidebar-menu-->
