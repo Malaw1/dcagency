5043c859e95452f1295bdd0a4741904b5135fa03
Djibril NDIAYE <djibsone51@gmail.com>
2018-05-07 15:53:33 +0000

    Changing some routes

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::group(['middleware' =>['auth']],function(){
 	Route::get('/admin/settings', 'AdminController@settings');
 	Route::get('/admin/check-pwd', 'AdminController@chkPassword');
 	Route::match(['get','post'], '/admin/update-pwd', 'AdminController@updatePassword');
+	Route::get('/admin/echantillons/index_echan', 'AdminController@index_echan');	
 	/*Route::get('admin/users', 'AdminController@user');*/
 
 	//Echantillons Routes (Admin)
@@ -38,3 +39,13 @@ Route::group(['middleware' =>['auth']],function(){
 
 Route::get('/logout', 'AdminController@logout');
 
+Route::get('/stocks', 'StocksController@index');
+Route::get('/users/listeperso', 'UsersController@index');
+Route::get('/substances', 'SubstancespuresController@index');
+
+Route::get('/logout', 'AdminController@logout');
+Route::resource('stocks', 'StocksController');
+Route::resource('echantillons', 'EchantilonsController');
+Route::resource('reactifs', 'ReactifsController');
+Route::resource('substancespures', 'SubstancespuresController');
+Route::resource('vaccins', 'VaccinsController');
