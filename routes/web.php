4d832f04ed0cc86cf35f5b80045317acf6484d49
Djibril NDIAYE <djibsone51@gmail.com>
2018-05-07 12:16:09 +0000

    Modifying the web.php page and the admin_side_bar panel

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,18 @@ Route::group(['middleware' =>['auth']],function(){
 	Route::match(['get','post'],'/admin/add-echantillon', 'EchantillonController@addEchanillon');
 	Route::match(['get','post'], '/admin/modifier-echantillon/{id}', 'EchantillonController@modifierEchantillons');
 	Route::get('/admin/voir-echantillon', 'EchantillonController@voirEchantillons');
-	});
+	Route::get('/stocks', 'StocksController@index');
+	Route::get('/users/listeperso', 'UsersController@index');
+  	Route::get('/substances', 'SubstancespuresController@index');
+
+});
+
+Route::get('/logout', 'AdminController@logout');
+Route::resource('stocks', 'StocksController');
+Route::resource('echantillons', 'EchantilonsController');
+Route::resource('reactifs', 'ReactifsController');
+Route::resource('substancespures', 'SubstancespuresController');
+Route::resource('vaccins', 'VaccinsController');
 
 Route::get('/logout', 'AdminController@logout');
 
