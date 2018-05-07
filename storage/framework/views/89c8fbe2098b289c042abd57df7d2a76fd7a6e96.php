ce68c3a2410cc277bad2270348d6678b1203f8ea
Djibril NDIAYE <djibsone51@gmail.com>
2018-05-07 14:31:03 +0000

    Something

--- storage/framework/views/89c8fbe2098b289c042abd57df7d2a76fd7a6e96.php
+++
@@ -1,7 +0,0 @@
-<!--Footer-part-->
-
-<div class="row-fluid">
-  <div id="footer" class="span12"> 2018 &copy; LIMS_LNCM Dakar,Sénégal <a href="http://lncm.com">Laboratoire national du contrôle des Médicaments</a> </div>
-</div>
-
-<!--end-Footer-part-->
