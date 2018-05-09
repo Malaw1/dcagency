ee15d25f93f77f0d1a304263b11b35726c13b8f9
Isergaye <ababacargaye3estm.edu.sn>
2018-05-09 15:11:26 +0000

    style admin_design et admin_sidebar

--- resources/views/layouts/adminLayout/admin_design.blade.php
+++ resources/views/layouts/adminLayout/admin_design.blade.php
@@ -12,6 +12,7 @@
 <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-media.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/backend_css/jquery.gritter.css') }}" /> 
 <link rel="stylesheet" href="{{ asset ('fonts/backend_fonts/css/font-awesome.css') }}" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso18.css" /> 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 </head>
