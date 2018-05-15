92c628e87a1dd257fbf916e42c7b40a123e025c8
Djibril NDIAYE <djibsone51@gmail.com>
2018-05-15 12:54:07 +0000

    hello World

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,13 +13,7 @@ class UsersController extends Controller
         $users = Poste::join('users', 'users.poste_id', '=', 'users.id')->select('users.*', 'postes.*')->get();
         //dd($poste);
 
-<<<<<<< HEAD
-        //$unites = Unite::all();
-        return view('users.listeperso', compact('users'));
-
-        //return view('/users/listeperso', ['users' => $users]);
-=======
-        //   $users = Poste::join('users', 'users.poste_id', '=', 'postes.id')->select('users.*', 'postes.*')->get();
+        //$users = Poste::join('users', 'users.poste_id', '=', 'postes.id')->select('users.*', 'postes.*')->get();
         //   //dd($users);
         // //$unites = Unite::all();
         // return view('users.listeperso', compact('users'));
@@ -40,6 +34,5 @@ class UsersController extends Controller
         $users = User::Where('id', $users->id)->get();
         dd($users);
         return view('users.show', ['users'=>$users]);
->>>>>>> 87eca860d9eb9e68bf12a3f0b42535c6438ee83d
     }
 }
